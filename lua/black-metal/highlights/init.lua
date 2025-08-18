455aed7878066ae1c321b419775f07e3bcea9451
Andrea Schiavini <metalelf0@gmail.com>
2025-08-18 14:33:42 +0200

    fix: allow setting custom colors (fixes #7)

--- lua/black-metal/highlights/init.lua
+++ lua/black-metal/highlights/init.lua
@@ -4,18 +4,18 @@ local M = {}
 
 ---@param highlights table<string,Highlight>
 local function vim_highlights(highlights)
-    for group, hi in pairs(highlights) do
-        vim.api.nvim_command(
-            string.format(
-                "highlight %s guifg=%s guibg=%s guisp=%s gui=%s",
-                group,
-                hi.fg or "none",
-                hi.bg or "none",
-                hi.sp or "none",
-                hi.fmt or "none"
-            )
-        )
-    end
+	for group, hi in pairs(highlights) do
+		vim.api.nvim_command(
+			string.format(
+				"highlight %s guifg=%s guibg=%s guisp=%s gui=%s",
+				group,
+				hi.fg or "none",
+				hi.bg or "none",
+				hi.sp or "none",
+				hi.fmt or "none"
+			)
+		)
+	end
 end
 
 ---Util for applying custom user colors.
@@ -24,59 +24,65 @@ end
 ---@param palette black-metal.Theme
 ---@return string
 local function overwrite(prefix, color, palette)
-    if not color then
-        return ""
-    end
-    if color:sub(1, 1) == "$" then
-        local name = color:sub(2, -1)
-        color = palette[name]
-        if not color then
-            vim.schedule(function()
-                vim.notify(
-                    'black-metal.nvim: unknown color "' .. name .. '"',
-                    vim.log.levels.ERROR,
-                    { title = "black-metal.nvim" }
-                )
-            end)
-            return ""
-        end
-    end
-    return prefix .. "=" .. color
+	if not color then
+		return ""
+	end
+	if color:sub(1, 1) == "$" then
+		local name = color:sub(2, -1)
+		color = palette[name]
+		if not color then
+			vim.schedule(function()
+				vim.notify(
+					'black-metal.nvim: unknown color "' .. name .. '"',
+					vim.log.levels.ERROR,
+					{ title = "black-metal.nvim" }
+				)
+			end)
+			return ""
+		end
+	end
+	return prefix .. "=" .. color
 end
 
 function M.setup()
-    ---@type black-metal.Config
-    local Config = require("black-metal").options()
-    ---@type black-metal.Theme
-    local c = require("black-metal.palette").get(Config.theme, Config.variant)
+	---@type black-metal.Config
+	local Config = require("black-metal").options()
+	---@type black-metal.Theme
+	local c = require("black-metal.palette").get(Config.theme, Config.variant)
 
-    local COMMON = require("black-metal.highlights.common").get()
-    local SYNTAX = require("black-metal.highlights.syntax").get()
-    local PLUGIN = require("black-metal.highlights.plugin").get()
+	local custom_colors = Config.colors
 
-    vim_highlights(COMMON)
-    for _, group in pairs(SYNTAX) do
-        vim_highlights(group)
-    end
-    for _, group in pairs(PLUGIN) do
-        vim_highlights(group)
-    end
+	for label, color in pairs(custom_colors) do
+		c[label] = color
+	end
 
-    for group, hi in pairs(Config.highlights) do
-        vim.api.nvim_command(
-            string.format(
-                "highlight %s %s %s %s %s",
-                group,
-                overwrite("guifg", hi.fg, c),
-                overwrite("guibg", hi.bg, c),
-                overwrite("guisp", hi.sp, c),
-                overwrite("gui", hi.fmt, c)
-            )
-        )
-    end
-    if Config.favor_treesitter_hl then
-        vim.highlight.priorities.semantic_tokens = 95
-    end
+	local COMMON = require("black-metal.highlights.common").get()
+	local SYNTAX = require("black-metal.highlights.syntax").get()
+	local PLUGIN = require("black-metal.highlights.plugin").get()
+
+	vim_highlights(COMMON)
+	for _, group in pairs(SYNTAX) do
+		vim_highlights(group)
+	end
+	for _, group in pairs(PLUGIN) do
+		vim_highlights(group)
+	end
+
+	for group, hi in pairs(Config.highlights) do
+		vim.api.nvim_command(
+			string.format(
+				"highlight %s %s %s %s %s",
+				group,
+				overwrite("guifg", hi.fg, c),
+				overwrite("guibg", hi.bg, c),
+				overwrite("guisp", hi.sp, c),
+				overwrite("gui", hi.fmt, c)
+			)
+		)
+	end
+	if Config.favor_treesitter_hl then
+		vim.highlight.priorities.semantic_tokens = 95
+	end
 end
 
 return M
